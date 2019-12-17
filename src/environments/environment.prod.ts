4c6dd58e2f8fdf8e935f08b73429cdb935212514
Luis Contreras <lacmdeveloper@gmail.com>
2019-12-17 18:32:53 -0300

    v30

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   app_name: 'FX10',
   api_url: 'https://fx11apiqa.primetec.cl',
-  appVersion: 'v1.0.29',
+  appVersion: 'v1.0.30',
   tz: 'America/Santiago'
 };

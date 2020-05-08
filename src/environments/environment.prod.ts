d29cbd93205d9654e730272c844c2b1924fc6a16
Luis Contreras <lacmdeveloper@gmail.com>
2020-05-08 09:17:14 -0400

    version updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: true,
   app_name: 'FX10',
   api_url: 'https://apiqa.fx360.cl',
-  appVersion: 'v1.0.60',
+  appVersion: 'v1.0.61',
   tz: 'America/Santiago',
   iosDeviceNames,
   syncTimer: 5

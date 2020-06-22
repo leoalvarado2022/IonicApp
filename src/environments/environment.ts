e0df5d7c8857199134d75813c22e54d6bfb882d4
Luis Contreras <lacmdeveloper@gmail.com>
2020-06-22 08:35:38 -0400

    *

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ export const environment = {
   production: false,
   app_name: 'FX10',
   api_url: 'http://127.0.0.1:5572',
-  appVersion: 'v1.0.82',
+  appVersion: 'v1.0.84',
   tz: 'America/Santiago',
   iosDeviceNames,
   syncTimerMinutes: 5

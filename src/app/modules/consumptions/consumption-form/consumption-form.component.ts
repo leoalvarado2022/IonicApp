201bab9da6a00b674a44048e56b29d59b9d62a65
Luis Contreras <lacmdeveloper@gmail.com>
2020-08-14 16:09:51 -0400

    *

--- src/app/modules/consumptions/consumption-form/consumption-form.component.ts
+++ src/app/modules/consumptions/consumption-form/consumption-form.component.ts
@@ -4,7 +4,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Warehouse, Product } from 'src/app/shared/services/store/store-interface';
 import { ConsumptionService } from '../services/consumption.service';
 import { Consumption } from './../../../shared/services/store/store-interface';
-import { find } from 'rxjs/operators';
 
 @Component({
   selector: 'app-consumption-form',
@@ -38,6 +37,10 @@ export class ConsumptionFormComponent implements OnInit {
   public filteredCostCenters: Array<any> = [];
   public costCenterName: string;
 
+  // UnitName
+  public unitCode: string;
+  public unitName: string;
+
   private readonly decimalRegex = /^\d*(.\d{1,3})?$/;
   public readonly actionHeader: any = {
     header: 'Seleccione',
@@ -58,14 +61,19 @@ export class ConsumptionFormComponent implements OnInit {
       this.consumptionForm = this.formBuilder.group({
         id: [ this.isCopy ? 0 : this.editConsumption.id, Validators.required],
         companyId: [this.editConsumption.companyId, Validators.required],
-        date: [this.editConsumption.date, Validators.required],
-        documentId: [this.editConsumption.documentId, Validators.required],
+        date: [this.editConsumption.date.split('T')[0], Validators.required],
+        documentId: [this.isCopy ? 0 : this.editConsumption.documentId, Validators.required],
         costCenterId: [this.editConsumption.costCenterId, Validators.required],
         warehouseOriginId: [this.editConsumption.warehouseOriginId, Validators.required],
         itemId: [this.editConsumption.itemId, Validators.required],
         itemName: [this.editConsumption.itemName, Validators.required],
+        unitId: [this.editConsumption.unitId, Validators.required],
         creatorId: [this.userId, Validators.required],
-        quantity: [this.editConsumption.quantity, Validators.required],
+        quantity: [this.isCopy ? 0 : this.editConsumption.quantity, [
+          Validators.required,
+          Validators.min(0.1),
+          Validators.pattern(this.decimalRegex)
+        ]],
         notes: [this.editConsumption.notes],
       });
 
@@ -81,18 +89,25 @@ export class ConsumptionFormComponent implements OnInit {
       // Product
       const findProduct = this.products.find(product => product.id === this.editConsumption.itemId);
       this.productName = findProduct.name;
+      this.unitCode = findProduct.unitCode;
+      this.unitName = findProduct.unitName;
     } else {
       this.consumptionForm = this.formBuilder.group({
         id: [0, Validators.required],
         companyId: [this.companyId, Validators.required],
-        date: [this.date, Validators.required],
+        date: [this.date.split('T')[0], Validators.required],
         documentId: [0, Validators.required],
         costCenterId: [0, Validators.required],
         warehouseOriginId: [0, Validators.required],
         itemId: [0, Validators.required],
         itemName: ['', Validators.required],
+        unitId: ['', Validators.required],
         creatorId: [this.userId, Validators.required],
-        quantity: ['', Validators.required],
+        quantity: ['', [
+          Validators.required,
+          Validators.min(0.1),
+          Validators.pattern(this.decimalRegex)
+        ]],
         notes: [''],
       });
     }
@@ -167,8 +182,11 @@ export class ConsumptionFormComponent implements OnInit {
   public cleanProductSearch = (): void => {
     this.consumptionForm.get('itemId').patchValue('');
     this.consumptionForm.get('itemName').patchValue('');
+    this.consumptionForm.get('unitId').patchValue('');
     this.filteredProducts = [];
     this.productName = null;
+    this.unitCode = null;
+    this.unitName = null;
   }
 
   /**
@@ -178,7 +196,10 @@ export class ConsumptionFormComponent implements OnInit {
   public selectProduct = (product: Product): void => {
     this.consumptionForm.get('itemId').patchValue(product.id);
     this.consumptionForm.get('itemName').patchValue(product.name);
+    this.consumptionForm.get('unitId').patchValue(product.unitId);
     this.productName = product.name;
+    this.unitCode = product.unitCode;
+    this.unitName = product.unitName;
     this.filteredProducts = [];
   }
 
@@ -236,6 +257,9 @@ export class ConsumptionFormComponent implements OnInit {
     return Object.assign({}, this.consumptionForm.value, {
       warehouseOriginCode: this.warehouseCode,
       warehouseOriginName: this.warehouseName,
+      costCenterName: this.costCenterName,
+      unitCode: this.unitCode,
+      unitName: this.unitName,
       tempId: this.tempId,
       status: 'new'
     });
@@ -260,6 +284,9 @@ export class ConsumptionFormComponent implements OnInit {
     return Object.assign({}, this.consumptionForm.value, {
       warehouseOriginCode: this.warehouseCode,
       warehouseOriginName: this.warehouseName,
+      costCenterName: this.costCenterName,
+      unitCode: this.unitCode,
+      unitName: this.unitName,
       tempId: this.editConsumption.tempId ? this.editConsumption.tempId : this.tempId,
       status: 'edit'
     });
@@ -284,6 +311,9 @@ export class ConsumptionFormComponent implements OnInit {
       tempId: this.tempId,
       warehouseOriginCode: this.warehouseCode,
       warehouseOriginName: this.warehouseName,
+      costCenterName: this.costCenterName,
+      unitCode: this.unitCode,
+      unitName: this.unitName,
       status: 'new'
     });
   }

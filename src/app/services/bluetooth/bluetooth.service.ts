18468f80776f9e39dc6c2f44f0e0452f4ff9c265
Luis Contreras <lacmdeveloper@gmail.com>
2020-08-19 10:41:23 -0400

    *

--- src/app/services/bluetooth/bluetooth.service.ts
+++ src/app/services/bluetooth/bluetooth.service.ts
@@ -4,7 +4,6 @@ import { timer } from 'rxjs/internal/observable/timer';
 import { BluetoothDevice } from './bluetooth-device.interface';
 import { ToastService } from 'src/app/shared/services/toast/toast.service';
 import { BehaviorSubject } from 'rxjs';
-import { takeLast, takeWhile, takeUntil } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -16,24 +15,24 @@ export class BluetoothService {
   private isSearchingDevices: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
   private lastWeight: BehaviorSubject<number> = new BehaviorSubject<number>(null);
 
-  private pairedDevices: BehaviorSubject<Array<BluetoothDevice>> = new BehaviorSubject<Array<BluetoothDevice>>([]);  
-  private availableDevices: BehaviorSubject<Array<BluetoothDevice>> = new BehaviorSubject<Array<BluetoothDevice>>([]);    
+  private pairedDevices: BehaviorSubject<Array<BluetoothDevice>> = new BehaviorSubject<Array<BluetoothDevice>>([]);
+  private availableDevices: BehaviorSubject<Array<BluetoothDevice>> = new BehaviorSubject<Array<BluetoothDevice>>([]);
 
   constructor(
     private bluetoothSerial: BluetoothSerial,
-    private toastService: ToastService    
+    private toastService: ToastService
   ) {
-    timer(0, 1000 * 5).subscribe( () => {      
-      this.bluetoothSerial.isEnabled().then( success => {
+    timer(0, 1000 * 5).subscribe(() => {
+      this.bluetoothSerial.isEnabled().then(success => {
         this.isBluetoothEnabled.next(success === 'OK');
 
         this.checkConnection();
-        this.listPairedDevices();        
+        this.listPairedDevices();
       }, error => {
         this.isBluetoothEnabled.next(false);
       });
     });
-  }  
+  }
 
   /**
    * getBluetoothStatus
@@ -59,21 +58,21 @@ export class BluetoothService {
   /**
    * getPairedDevices
    */
-  public getPairedDevices ():  BehaviorSubject<Array<BluetoothDevice>> {
+  public getPairedDevices(): BehaviorSubject<Array<BluetoothDevice>> {
     return this.pairedDevices;
   }
 
   /**
    * getAvailableDevices
    */
-  public getAvailableDevices ():  BehaviorSubject<Array<BluetoothDevice>> {
+  public getAvailableDevices(): BehaviorSubject<Array<BluetoothDevice>> {
     return this.availableDevices;
   }
 
   /**
    * getLastWeight
    */
-  public getLastWeight ():  BehaviorSubject<number> {
+  public getLastWeight(): BehaviorSubject<number> {
     return this.lastWeight;
   }
 
@@ -81,37 +80,32 @@ export class BluetoothService {
    * checkConnection
    */
   private checkConnection = () => {
-    this.bluetoothSerial.isConnected().then( data => {
-      console.log('checkConnection', data);
-      this.isDeviceConnected.next( !(data === 'Not connected'));      
-    }, error => {      
-      console.log('checkConnection', error);
+    this.bluetoothSerial.isConnected().then(data => {
+      this.isDeviceConnected.next(data === 'OK');
+    }, error => {
       this.isDeviceConnected.next(false);
     });
-  }  
+  }
 
   /**
    * listPairedDevices
    */
   private listPairedDevices = () => {
-    this.bluetoothSerial.list().then( data => {      
+    this.bluetoothSerial.list().then(data => {
       this.pairedDevices.next(data);
     }, error => {
       this.pairedDevices.next([]);
     });
-  }  
+  }
 
   /**
    * connectDevice
    * @param device 
    */
-  public connectDevice = (device: BluetoothDevice) => {    
-    console.log('connectDevice start');
+  public connectDevice = (device: BluetoothDevice) => {
     this.bluetoothSerial.connect(device.address).subscribe(success => {
-      console.log('connectDevice success', success);            
       this.toastService.successToast('Dispositivo Conectado');
     }, error => {
-      console.log('connectDevice error', error);      
       this.toastService.errorToast('Ocurrio un error al conectar el dispositivo');
     });
   }
@@ -119,12 +113,11 @@ export class BluetoothService {
   /**
    * disconnectDevice
    */
-  public disconnectDevice = () => {    
+  public disconnectDevice = () => {
     this.bluetoothSerial.disconnect().then(success => {
       this.listPairedDevices();
       this.toastService.warningToast('Dispositivo Desconectado');
     }, error => {
-      console.log('error', error);
       this.listPairedDevices();
       this.toastService.errorToast('Ocurrio un error desconectando dispositivo');
     });
@@ -133,18 +126,18 @@ export class BluetoothService {
   /**
    * scanDevices
    */
-  public scanDevices = () => {     
+  public scanDevices = () => {
     this.isSearchingDevices.next(true);
-    this.availableDevices.next([]);    
+    this.availableDevices.next([]);
 
-    this.bluetoothSerial.discoverUnpaired().then( (data: Array<BluetoothDevice>) => {                  
+    this.bluetoothSerial.discoverUnpaired().then((data: Array<BluetoothDevice>) => {
       this.availableDevices.next(data);
       this.isSearchingDevices.next(false);
     }, error => {
       this.availableDevices.next([]);
       this.isSearchingDevices.next(false);
     });
-  }   
+  }
 
   /**
    * getDeviceData
@@ -154,7 +147,7 @@ export class BluetoothService {
     let counter = 0;
     let data: Array<any> = [];
 
-    this.bluetoothSerial.subscribe('\n').subscribe( data => {            
+    this.bluetoothSerial.subscribe('\n').subscribe(data => {
       if (last === null) {
         last = data;
       }
@@ -166,20 +159,19 @@ export class BluetoothService {
         last = null;
       }
 
-      if(counter === 10) {
-        console.log('envia', last);
+      if (counter === 10) {
         this.lastWeight.next(this.processWeight(last));
         last = null;
         counter = 0;
       }
     });
-  }     
+  }
 
   /**
    * processWeight
    * @param value 
    */
-  private processWeight = (value: string): number => {    
+  private processWeight = (value: string): number => {
     const noSpaces = value.replace(/\s/g, '');
     const weightString = noSpaces.split(",")[2];
     const cleanWeight = weightString.replace('kg', '');
@@ -191,8 +183,8 @@ export class BluetoothService {
    * enableBlueetooth
    */
   public enableBlueetooth = () => {
-    this.bluetoothSerial.enable().then( () => {
-      
+    this.bluetoothSerial.enable().then(() => {
+
     }, error => {
 
     });

9cb2ac6b7202ca96760074d5ef628afbeb56ddab
Luis Contreras <lacmdeveloper@gmail.com>
2020-12-03 09:28:31 -0300

    Integrado servicio del clima Aplicada validacion haversine de que solo tome posiciones de 5 metros o mas

--- src/app/modules/application-registry/application-start/application-start.page.ts
+++ src/app/modules/application-registry/application-start/application-start.page.ts
@@ -1,38 +1,39 @@
-import { AgmMap } from '@agm/core';
-import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Geoposition } from '@ionic-native/geolocation/ngx';
 import { Subject } from 'rxjs';
-import { takeUntil, filter, map } from 'rxjs/operators';
+import { takeUntil, filter, map, take } from 'rxjs/operators';
 import { OrderSyncService } from 'src/app/services/storage/order-sync/order-sync.service';
+import { WeatherService } from 'src/app/services/weather/weather.service';
 import { GeolocationService } from 'src/app/shared/services/geolocation/geolocation.service';
 import { ApplicationListInterface } from '../application-list.interface';
 import { ApplicationLocationInterface } from '../application-location.interface';
-
-declare var google: any;
-
+import * as haversine from "haversine";
+import { ToastService } from 'src/app/shared/services/toast/toast.service';
 @Component({
   selector: 'app-application-start',
   templateUrl: './application-start.page.html',
   styleUrls: ['./application-start.page.scss'],
 })
 export class ApplicationStartPage implements OnInit, OnDestroy {
-  
-  public currentLat = -33.4372; // Santiago Default
-  public currentLng = -70.6506; // Santiago Default
-  public zoom = 10;
 
   public currentApplication: ApplicationListInterface;
-  public positions: Array<ApplicationLocationInterface> = [];  
+  public positions: Array<ApplicationLocationInterface> = [];
   private id: number;
   private tempId: number;
   private unsubscriber = new Subject();
+
+  public weather: any;
+  public loading = false;
+  public loadingMessage = "Cargando...";
 
   constructor(
     private route: ActivatedRoute,
     private orderSyncService: OrderSyncService,
     private geolocationService: GeolocationService,
-    private router: Router
+    private router: Router,
+    private weatherService: WeatherService,
+    private toastService: ToastService
   ) {
 
   }
@@ -49,40 +50,79 @@ export class ApplicationStartPage implements OnInit, OnDestroy {
       this.positions = data[1];
 
       this.tempId = data[2];
-    });    
+    });
   }
 
   ngOnDestroy() {
     this.unsubscriber.complete();
   }
 
-  ionViewDidEnter() {    
+  ionViewDidEnter() {
+    this.loading = true;
+    this.loadingMessage = "Obteniendo Ubicacion";
+
     this.geolocationService.watchPosition().pipe(
       takeUntil(this.unsubscriber),
-      filter((position: Geoposition) => position.coords !== undefined && (position.coords.latitude !== this.currentLat && position.coords.longitude !== this.currentLng)),
-      map(item => {
-        return {
-          id: this.id,
-          timestamp: item.timestamp,
-          latitude: item.coords.latitude,
-          longitude: item.coords.longitude,
-          accuracy: item.coords.accuracy,
-          altitude: item.coords.altitude,
-          altitudeAccuracy: item.coords.altitudeAccuracy,
-          heading: item.coords.heading,
-          speed: item.coords.speed,
-          tempId: this.tempId
-        };
-      })
+      filter((position: Geoposition) => position.coords !== undefined),
+      map(item => this.mapCustomPosition(item)),
+      take(1)
     ).subscribe((geoposition: ApplicationLocationInterface) => {
-      this.orderSyncService.addApplicationLocations(geoposition).then((data: Array<ApplicationLocationInterface>) => {
-        this.positions = [...data];        
+      this.loadingMessage = "Obteniendo Clima";
 
-        this.currentLat = geoposition.latitude;
-        this.currentLng = geoposition.longitude;        
+      this.weatherService.getLatLngWeather(geoposition.latitude, geoposition.longitude).subscribe(weather => {
+        this.weather = weather["data"];
+        this.loading = false;
+
+        this.watchPosition();
+      }, error => {
+        this.toastService.errorToast('No se pudo cargar el clima');
+        this.loading = false;
       });
     })
   }
+
+  /**
+   * watchPosition
+   */
+  private watchPosition = () => {
+    this.geolocationService.watchPosition().pipe(
+      takeUntil(this.unsubscriber),
+      filter((position: Geoposition) => position.coords !== undefined),
+      map(item => this.mapCustomPosition(item))
+    ).subscribe((geoposition: ApplicationLocationInterface) => {
+      if (this.positions.length === 0) {
+        this.orderSyncService.addApplicationLocations(geoposition).then((data: Array<ApplicationLocationInterface>) => {
+          this.positions = [...data];
+        });
+      } else if (this.positions.length > 0) {
+        const start = this.positions[this.positions.length - 1];
+        const distance = haversine(start, geoposition, { unit: 'meter' });
+
+        if (distance > 5) {
+          this.orderSyncService.addApplicationLocations(geoposition).then((data: Array<ApplicationLocationInterface>) => {
+            this.positions = [...data];
+          });
+        }
+      }
+    })
+  }
+
+  /**
+   * mapCustomPosition
+   * @param item geoposition object
+   */
+  private mapCustomPosition = (item: any) => ({
+    id: this.id,
+    timestamp: item.timestamp,
+    latitude: item.coords.latitude,
+    longitude: item.coords.longitude,
+    accuracy: item.coords.accuracy,
+    altitude: item.coords.altitude,
+    altitudeAccuracy: item.coords.altitudeAccuracy,
+    heading: item.coords.heading,
+    speed: item.coords.speed,
+    tempId: this.tempId
+  });
 
   /**
    * endApplication

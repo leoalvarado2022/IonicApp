73a1ac3921db9b2d3d55ed0016e0b571d3427a65
Luis Contreras <lacmdeveloper@gmail.com>
2021-01-12 09:40:02 -0300

    *

--- src/app/modules/crm/ticket-details-list/ticket-detail-card/ticket-detail-card.component.ts
+++ src/app/modules/crm/ticket-details-list/ticket-detail-card/ticket-detail-card.component.ts
@@ -1,4 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-ticket-detail-card',
@@ -9,7 +11,10 @@ export class TicketDetailCardComponent implements OnInit {
 
   @Input() detail: any;
 
-  constructor() {
+  constructor(
+    private inAppBrowser: InAppBrowser,
+    private platform: Platform
+  ) {
 
   }
 
@@ -17,16 +22,30 @@ export class TicketDetailCardComponent implements OnInit {
 
   }
 
-
   /**
    * buildLink
-   * @param file
+   * @param file to build link
    */
-  public buildLink = (file: any): string => {
+  private buildLink = (file: any): string => {
     if (file.link && file.link !== 'undefined') {
       return `https://drive.google.com/file/d/${file.link}/view?usp=sharing`;
     }
 
     return '#';
   }
+
+  /**
+   * openLink
+   * @param file to open 
+   */
+  public openLink = (file: any): void => {
+    const url = this.buildLink(file);
+
+    if (this.platform.is('mobile')) {
+      this.inAppBrowser.create(url, '_blank');
+    } else {
+      window.open(url, "_blank");
+    }
+  }
+
 }

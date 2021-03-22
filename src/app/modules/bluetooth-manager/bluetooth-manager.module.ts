50795303444bcfdfbc12a17a31e6ee2c62ad4fec
Andres Olivares <olivaresdeveloper@gmail.com>
2021-03-22 17:38:22 -0300

    actualizacion

--- src/app/modules/bluetooth-manager/bluetooth-manager.module.ts
+++ src/app/modules/bluetooth-manager/bluetooth-manager.module.ts
@@ -3,17 +3,11 @@ import {NgModule} from '@angular/core';
 import {BluetoothManagerPageRoutingModule} from './bluetooth-manager-routing.module';
 import {BluetoothManagerPage} from './bluetooth-manager.page';
 import {SharedModule} from 'src/app/shared/shared.module';
-import {BluetoothSerial} from '@ionic-native/bluetooth-serial/ngx';
-import {BluetoothService} from '../../services/bluetooth/bluetooth.service';
 
 @NgModule({
   imports: [
     SharedModule,
     BluetoothManagerPageRoutingModule
-  ],
-  providers: [
-    BluetoothSerial,
-    BluetoothService
   ],
   declarations: [BluetoothManagerPage]
 })
--- src/app/modules/tratos/tratos-list/tratos-list.module.ts
+++ src/app/modules/tratos/tratos-list/tratos-list.module.ts
@@ -10,7 +10,6 @@ import {HttpClientModule} from '@angular/common/http';
 import {DealsService} from '../services/deals/deals.service';
 import {TratosScannedPage} from '../tratos-scanned/tratos-scanned.page';
 import {WorkerDealsComponent} from '../worker-deals/worker-deals.component';
-import {BluetoothSerial} from '@ionic-native/bluetooth-serial/ngx';
 
 @NgModule({
   imports: [
@@ -19,7 +18,7 @@ import {BluetoothSerial} from '@ionic-native/bluetooth-serial/ngx';
     HttpClientModule
   ],
   declarations: [TratosListPage, AddTratoPage, AddCenterCostPage, TratosScannedPage, WorkerDealsComponent],
-  providers: [DealsService, BluetoothSerial, BluetoothSerial]
+  providers: [DealsService]
 })
 export class TratosListPageModule {
 }
